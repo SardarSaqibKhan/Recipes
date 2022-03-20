ff58100c30317a3767b4e293aa3f8f3796e65800
Sardar Saqib <sardarsaqib294@gmail.com>
2022-03-20 13:04:26 +0000

    Updated virsion

---
+++ Recipes/CategoryCell.swift
@@ -0,0 +1,13 @@
+//
+//  CategoryCell.swift
+//  Enebla
+//
+//  Created by Sardar Saqib on 08/05/2020.
+//  Copyright © 2020 FV iMAGINATION. All rights reserved.
+//
+
+import UIKit
+
+class CategoryCell: UICollectionViewCell {
+    
+}
---
+++ Recipes/ShoppingListTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ShoppingListTableViewCell.swift
+//  Enebla
+//
+//  Created by Sardar Saqib on 12/05/2020.
+//  Copyright © 2020 FV iMAGINATION. All rights reserved.
+//
+
+import UIKit
+
+class ShoppingListTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
